32c1c5e8936c9d08afedcce68d124c0a2fdae8a6
Zach Baker <zach.r.baker1@gmail.com>
2016-11-17 20:34:58 -0700

    working with reactcss

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import reactCSS from 'reactcss';
 
 export default class Home extends React.Component {
+
 	render () {
+		const styles = reactCSS({
+			'default': {
+				title: {
+					fontSize: '20px',
+					color: 'green'
+				}
+			}
+		})
 		return (
 			<div>
-				HOMIES
+				<h1 style={ styles.title }>HOMIES</h1>
 			</div>
 		)
 	}
