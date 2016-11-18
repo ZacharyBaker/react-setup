ba3e929a8ab09a16a7f212e1b3e34496890969c0
Zach Baker <zach.r.baker1@gmail.com>
2016-11-17 23:36:13 -0700

    Reverting to the state of the project before movietime

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import reactCSS from 'reactcss';
-import MovieGetter from './MovieGetter';
 
 export default class Home extends React.Component {
-
 	render () {
-		const styles = reactCSS({
-			'default': {
-				title: {
-					fontSize: '20px',
-					color: 'blue'
-				}
-			}
-		})
 		return (
 			<div>
-				<h1 style={ styles.title }>Movie Time Breh</h1>
-				<MovieGetter />
+				HOMIES
 			</div>
 		)
 	}
